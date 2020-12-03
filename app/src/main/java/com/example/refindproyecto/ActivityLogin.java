53b1dc8ea355a478249024a4f57d4bddc1ae1486
AlvaroOrtiz <alorpes@gmail.com>
2020-12-03 02:01:43 +0100

    Login con FireBase Listo

--- app/src/main/java/com/example/refindproyecto/ActivityLogin.java
+++ app/src/main/java/com/example/refindproyecto/ActivityLogin.java
@@ -1,14 +1,60 @@
 package com.example.refindproyecto;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ActivityLogin extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private EditText correo;
+    private EditText pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        correo = findViewById(R.id.emailLogin);
+        pass = findViewById(R.id.passLogin);
+        mAuth = FirebaseAuth.getInstance();
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+    public void toRegistro (View view){
+        Intent registro = new Intent(this, ActivityRegistro.class);
+        startActivity(registro);
+    }
+    public void iniciarSesion (View view){
+        //TODO : pendiente hacer comprobar que los apartado no esten vacios y hacer un boton de logout.
+        mAuth.signInWithEmailAndPassword(correo.getText().toString().trim(), pass.getText().toString().trim())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(i);
+                            Toast.makeText(getApplicationContext(), "Inicio de sesion correcto.",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Fallo al iniciar sesion.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
