ae8288c9f3b57ff128c90c96d5db2d9d873f231a
AlvaroOrtiz <alorpes@gmail.com>
2020-12-03 02:12:38 +0100

    prueba rama

--- app/src/main/java/com/example/refindproyecto/MainActivity.java
+++ app/src/main/java/com/example/refindproyecto/MainActivity.java
@@ -10,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Todo: para hacer
+        /**
+         * Videos sobre el menu en la parte inferior
+         * https://www.youtube.com/watch?v=Z67AJVEAl_4
+         * https://www.youtube.com/watch?v=tPV8xA7m-iw
+         */
+        //Todo: firebase
+        //https://console.firebase.google.com/u/0/project/refindp-6ddc1/overview
     }
 }
