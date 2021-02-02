180ae08100bc044a0bc388f65f3704f5c3915faf
alorp <alorpes@gmail.com>
2021-02-02 12:21:10 +0100

    Seguridad para presentacion

--- app/src/main/java/com/example/refindproyecto/ActivityFavoritos.java
+++ app/src/main/java/com/example/refindproyecto/ActivityFavoritos.java
@@ -1,18 +1,27 @@
 package com.example.refindproyecto;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.refindproyecto.Modelo.Favorito;
+
+import org.w3c.dom.Text;
 
 public class ActivityFavoritos extends AppCompatActivity {
     ImageButton btnInicio, btnFavorito, btnPerfil;
     Favorito favorito = new Favorito();
     TextView tvTitulo;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +31,8 @@ public class ActivityFavoritos extends AppCompatActivity {
         btnFavorito =findViewById(R.id.btnFavorito);
         btnPerfil =findViewById(R.id.btnPerfil);
         tvTitulo = findViewById(R.id.tvTitulo);
-
+        TextView tvMenuContextual  = findViewById(R.id.tvTitulo);
+        registerForContextMenu(tvMenuContextual);
 
         btnInicio.setOnClickListener(v -> {
 
@@ -47,6 +57,27 @@ public class ActivityFavoritos extends AppCompatActivity {
         else{
             tvTitulo.setText("Todavia no tienes ningun Anuncio en favoritos");
         }
+
+
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.menu_contextual, menu);
     }
 
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.opcion1:
+                Toast.makeText(this, "Esperar es sabio", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.opcion2:
+                Toast.makeText(this, "Error de capa 8", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+
+    }
 }
--- app/src/main/java/com/example/refindproyecto/MapsActivity.java
+++ app/src/main/java/com/example/refindproyecto/MapsActivity.java
@@ -1,12 +1,8 @@
 package com.example.refindproyecto;
 
 import androidx.fragment.app.FragmentActivity;
-
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.example.refindproyecto.Modelo.Anuncio;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -14,29 +10,29 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class ActivityAnuncio extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
-    private ImageView imgItemDetail;
-    private TextView tvTituloDetail;
-    private TextView tvDescripcionDetail;
-    private Anuncio datosAnuncio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_anuncio);
+        setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-        datosAnuncio = (Anuncio) getIntent().getExtras().getSerializable("datosanuncio");
-        initViews();
-        initValues();
     }
 
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -45,16 +41,5 @@ public class ActivityAnuncio extends FragmentActivity implements OnMapReadyCallb
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-    private void initViews() {
-        imgItemDetail = findViewById(R.id.imgItemDetail);
-        tvTituloDetail = findViewById(R.id.tvTituloDetail);
-        tvDescripcionDetail = findViewById(R.id.tvDescripcionDetail);
-    }
-
-    private void initValues(){
-        imgItemDetail.setImageResource(datosAnuncio.getImgResource());
-        tvTituloDetail.setText(datosAnuncio.getTitulo());
-        tvDescripcionDetail.setText(datosAnuncio.getDescripcion());
     }
 }
